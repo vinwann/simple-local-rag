0a4d77102814810ea58c626c205412e405feb5b8
vinwann <72453709+vinwann@users.noreply.github.com>
2024-08-11 07:44:10 +0530

    gpu check

--- check_gpu.py
+++ check_gpu.py
@@ -1,2 +1,6 @@
 import torch
 
+if torch.cuda.is_available():
+    print(f"CUDA is available. GPU device: {torch.cuda.get_device_name(0)}")
+else:
+    print("CUDA is not available. Running on CPU.")
